37dc9e204019d3ffaaa909f3ce86ec69a9de8078
arianna choza <unachoza@gmail.com>
2024-04-22 16:57:35 -0400

    about page copy added plus fonts

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,5 +1,21 @@
+import data from "../data.json";
+import "./Pages.css";
+
 const About = () => {
-	return <>About</>;
+	return (
+		<>
+			{data.map((section, i) => {
+				const title = Object.keys(section)[0];
+				const content = Object.values(section)[0];
+				return (
+					<div key={i} className="section">
+						<div className="title">{title}</div>
+						<div className="content">{content}</div>
+					</div>
+				);
+			})}
+		</>
+	);
 };
 
 export default About;
